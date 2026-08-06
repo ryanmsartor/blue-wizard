a1be4ab01703833dd1b1821935a2565ebecc4c94
Ryan Sartor <89538678+ryanmsartor@users.noreply.github.com>
2026-08-06 00:51:32 -0500

    fix switched score labels

--- scripts/GameOverScreen.gd
+++ scripts/GameOverScreen.gd
@@ -9,8 +9,8 @@ extends ColorRect
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	get_tree().set_pause(true)
-	$HighScoreLabel.text = "High Score: " + str(Global.score)
-	$FinalScoreLabel.text = "Final Score: " + str(Global.high_score)
+	$HighScoreLabel.text = "High Score: " + str(Global.high_score)
+	$FinalScoreLabel.text = "Final Score: " + str(Global.score)
 	$StartButton.grab_focus()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
